92cc680d85db3006c951a7528cc5973fa2a26c70
NicLambie <68138996+NicLambie@users.noreply.github.com>
2020-11-17 19:43:05 +1000

    Test commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Test Edit <code>src/App.js</code> and save to reload.
+          Test Edit new commit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
