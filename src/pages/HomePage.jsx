350312ec01576c7d8f86973b83822ed9795274a9
NicLambie <68138996+NicLambie@users.noreply.github.com>
2020-11-21 10:04:43 +1000

    Update HomePage.jsx
    Testing text

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 function HomePage() {
-    return <h1>This is the home page.</h1>;
-    }
+  return <h1>This is the home page - it really is.</h1>;
+}
 export default HomePage;
