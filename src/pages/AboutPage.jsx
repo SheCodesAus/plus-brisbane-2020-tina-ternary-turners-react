44c41c29b7443e3821a37479813a30760e2f852f
RebeccaMillwood <bec-millwood@live.com.au>
2020-11-24 19:26:22 +1000

    moved home page text to about page

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,5 +1,44 @@
 import React from "react";
 function AboutPage() {
-  return <h1>This is the about page.</h1>;
+  return (
+    <div id="info">
+      <h1>Welcome to Income Splitter</h1>
+      <p>
+        Want to manage your income more effectively? Tired of working out how
+        much you need for tax, bills and other expenses? Always running out of
+        money before next pay day?
+      </p>
+      <p>
+        Join Income Splitter today and say goodbye to money headaches. Income
+        Splitter allows you to put your income into ‘buckets’ each time you’re
+        paid, making sure you have enough to pay your bills - and enjoy your
+        lifestyle.
+      </p>
+      <p>
+        Designed specifically for workers and contractors on the go, you can
+        spend less time managing your money and more time managing your
+        business!
+      </p>
+      <h1>Join today!</h1>
+      <p>3 simple steps to get started!</p>
+
+      <h2>Step 1</h2>
+      <p>
+        Start by working out where your money needs to go each month – things
+        like bills, savings and fun stuff. These will be your buckets.​{" "}
+      </p>
+
+      <h2>Step 2</h2>
+      <p>
+        {" "}
+        Decide how much of your money goes into each bucket, each time you are
+        paid.
+      </p>
+
+      <h2>Step 3</h2>
+      <p> Set up your buckets in Income Splitter and you’re good to go! </p>
+      <h1>Get Started (Register button to go here)</h1>
+    </div>
+  );
 }
 export default AboutPage;
