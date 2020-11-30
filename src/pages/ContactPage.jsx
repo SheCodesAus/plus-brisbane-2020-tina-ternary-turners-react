14b04c12886f5c87093adede53576bcc3ee1d4ae
RebeccaMillwood <bec-millwood@live.com.au>
2020-11-30 21:43:57 +1000

    proof read and made edits to some text, started work on contact page

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,5 +1,14 @@
 import React from "react";
+
 function ContactPage() {
-return <h1>This is the contact page.</h1>;
+return (
+    <div>
+        <h1>Contact us by following the links below to our LinkedIn pages: </h1>
+            <h2><a href="https://www.linkedin.com/in/nicolalambie/">Nicola</a></h2>
+            <h2><a href="https://www.linkedin.com/in/rebeccamillwood/">Bec</a></h2>
+            <h2><a href="https://www.linkedin.com/in/elnaz-karimpour-49a7a877/">Elnaz</a></h2>
+            <h2><a href="https://www.linkedin.com/in/rachael-dagge-798334133/">Rachael</a></h2>
+</div>
+    )
 }
 export default ContactPage;
